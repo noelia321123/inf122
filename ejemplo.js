dd767f74de1e8324d59019590235a1e1394bb4ef
noelia321123 <noeliaUM180@gmail.com>
2024-01-08 22:21:49 -0400

    clase

---
+++ ejemplo.js
@@ -0,0 +1,25 @@
+var encontrado = colores.find(function (elemnto){
+    return elemento==="rojo"
+
+});
+
+var elemento =colores.findIndex(function(elemento){
+    return elemento === 1;
+});
+
+
+//elemplo 2//
+function saludar(nombre){
+    return"hola",+nombre+"!";
+
+}
+const mensaje = saludar("noelia")
+
+//ejemplo 3//
+function sumar( sum1, sum2)
+{
+    let resultado = sum1+ sum2;
+    return resultado;
+}
+
+alert ("4 + 10 ="+ sumar(4, 10));
